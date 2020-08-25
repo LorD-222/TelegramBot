17ae7f7896b4e1d8678a81ce8b6d1caaafbaf3d7
LorD_222 <ilya.r.200594@gmail.com>
2020-08-25 10:04:00 +0300

    add sql scripts

--- bot.py
+++ bot.py
@@ -2,39 +2,42 @@ import config
 import telebot
 from telebot import apihelper
 import urllib.request 
-import pyodbc
-from datetime import datetime
+import pyodbc # для взаимодействия с БД
+import pandas as pd # для манипулирования данными и экспорта в виде электронных таблиц
+
 
 driver = 'DRIVER={SQL Server}'
-server = 'SERVER=dc-sql12-db\\db'
-db = 'DATABASE=Inventory'
-conn_str = ';'.join([driver, server, db])
- 
-conn = pyodbc.connect(conn_str)
-cursor = conn.cursor()
- 
-cursor.execute("SELECT * FROM dbo.main WHERE hostname = '90514-ws404' ")
-row = cursor.fetchone()
-rest_of_rows = cursor.fetchall()
-for item in rest_of_rows:
-   print(item)
+server = 'SERVER=dc-sql12-db\\db' # имя сервера
+db = 'DATABASE=Inventory' # имя БД
+conn_str = ';'.join([driver, server, db]) 
+conn = pyodbc.connect(conn_str) # Подключение БД
+
+script = """
+SELECT * FROM dbo.main WHERE hostname = '90514-ws404'
+""" # скрипт запроса к БД
+
+df = pd.read_sql_query(script, conn) # чтение полученых данных с занесением в переменную
+
+writer = pd.ExcelWriter('documents\\example.xlsx') # создание файла
+df.to_excel(writer, sheet_name='bar') # Добавление данных из переменной с созданием листа
+writer.save() # сохранение
 
 bot_kz = telebot.TeleBot(config.token)
 keyboard1 = telebot.types.ReplyKeyboardMarkup(True, True)
 keyboard1.row('Привет', 'Пока')
 keyboard1.row('пошел', 'на')
 
-@bot_kz.message_handler(commands=['start'])
+@bot_kz.message_handler(commands=['start']) # Реакция на команду /
 def start_message(message):
     bot_kz.send_message(message.chat.id, 'Привет, надеюсь ты найдешь здесь чтонибудь полезное,' + message.from_user.first_name + ', или иди нахуй!', reply_markup=keyboard1)
 
-@bot_kz.message_handler(commands=['help'])  
+@bot_kz.message_handler(commands=['help'])  # Реакция на команду /
 def help_command(message):  
     keyboard = telebot.types.InlineKeyboardMarkup()  
     keyboard.add(telebot.types.InlineKeyboardButton('Не пиши мне', url='https://telegram.me/LorD_222'))
     bot_kz.send_message(message.chat.id,'Да-да я создал', reply_markup=keyboard)
 
-@bot_kz.message_handler(content_types=['text'])
+@bot_kz.message_handler(content_types=['text']) # Реакция на текст
 def send_text(message):
    if message.text.lower() == 'привет':
         bot_kz.send_message(message.chat.id, 'Привет, ' + message.from_user.first_name)
@@ -50,15 +53,16 @@ def send_text(message):
         bot_kz.send_document(message.chat.id, file)
 
 
-@bot_kz.message_handler(content_types=["document"])
+@bot_kz.message_handler(content_types=["document"]) # Документ
 def handle_docs_audio(message):
+    # Получим ID Стикера
     document_id = message.document.file_id
+    # Нужно получить путь, где лежит файл на Сервере Телеграмма
     file_info = bot_kz.get_file(document_id)
+    # Теперь формируем ссылку и скачивам файл
     urllib.request.urlretrieve(f'http://api.telegram.org/file/bot{config.token}/{file_info.file_path}', file_info.file_path)
 
-
-
-@bot_kz.message_handler(content_types=["sticker"])
+@bot_kz.message_handler(content_types=["sticker"]) # Скачать стикер
 def handle_docs_audio(message):
     # Получим ID Стикера
     sticker_id = message.sticker.file_id
--- test_sql.py
+++ test_sql.py
@@ -4,6 +4,16 @@ from telebot import apihelper
 import urllib.request 
 import pyodbc
 from datetime import datetime
+import openpyxl
+
+# создаем новый excel-файл
+wb = openpyxl.Workbook()
+
+# добавляем новый лист
+#wb.create_sheet(title = 'Первый лист', index = 0)
+
+# получаем лист, с которым будем работать
+sheet = wb.active
 
 driver = 'DRIVER={SQL Server}'
 server = 'SERVER=dc-sql12-db\\db'
@@ -13,8 +23,12 @@ conn_str = ';'.join([driver, server, db])
 conn = pyodbc.connect(conn_str)
 cursor = conn.cursor()
  
-cursor.execute("SELECT * FROM dbo.main WHERE hostname = '90514-ws404' ")
+cursor.execute("SELECT hostname FROM dbo.main WHERE hostname = '90514-ws404' ")
 row = cursor.fetchone()
 rest_of_rows = cursor.fetchall()
-for item in rest_of_rows:
-   print(item)
+#for item in rest_of_rows:
+c1 = sheet.cell(row = 1, column = 1)
+print(row)
+c1.value = row
+#   print(item)
+wb.save('documents\\example.xlsx')
